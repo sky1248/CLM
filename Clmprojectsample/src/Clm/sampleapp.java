9cb0aeca6f4f10f17ae526f6db8fd72b64a2257f
Rushcode MMV <Rushcode MMV@DESKTOP-2SNU8KS>
2018-11-22 17:21:43 +0530

    second commit upload sameproject with extra line

--- Clmprojectsample/src/Clm/sampleapp.java
+++ Clmprojectsample/src/Clm/sampleapp.java
@@ -4,7 +4,9 @@ public class sampleapp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("RTC Sky ");
+System.out.println("RTC Sky 1st commit ");
+System.out.println("2nd commit");
+
 	}
 
 }
